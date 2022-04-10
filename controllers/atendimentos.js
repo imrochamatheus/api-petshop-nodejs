89dbec4ad4c257ae89971240b1d7642d0ee7959b
imrochamatheus <im.rochamatheus@gmail.com>
2022-04-10 17:59:19 -0300

    Iniciando projeto

---
+++ controllers/atendimentos.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => {
+    res.send('Você está na rota de atendimentos e está fazendo um get')
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos e está fazendo um post')
+    })
+})}
