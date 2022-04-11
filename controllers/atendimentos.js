557dfac3a2ffc9ca81474aa104d7b6b6e8b01808
Matheus <im.rochamatheus@gmail.com>
2022-04-11 08:41:16 -0300

    :sparkles: Implementada requisição de criação de agendamento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => {
-    res.send('Você está na rota de atendimentos e está fazendo um get')
-
+        res.send('Você está na rota de atendimentos e está fazendo um get')
+    },
+    
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('Você está na rota de atendimentos e está fazendo um post')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento, res)
     })
-})}
+)}
